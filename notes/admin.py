24e2423f5436e7f0249a68df85b762d0896cd19c
PathumSandeepa <pathumsandeepa13@gmail.com>
2026-03-30 18:01:57 +0530

    [nexus-03] Register Note model in Django admin with list display and search

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at')
+    search_fields = ('title', 'description')
+    readonly_fields = ('id', 'created_at', 'updated_at')
